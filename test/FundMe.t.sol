f963f1d730fe685e19d133bd5a862208006b38e5
rbd3 <rabedesanaandry01@gmail.com>
2025-02-06 11:08:52 +0300

    Update test

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -2,15 +2,31 @@
 
 pragma solidity 0.8.28;
 import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+error NotOwner();
 
 contract FundMeTest is Test {
     uint256 number = 1;
+    FundMe fundMe;
     function setUp() external {
         number = 2;
+        fundMe = new FundMe();
     }
     function testDemo() public view {
         console.log(number);
         console.log("Hi guys");
         assertEq(number, 2);
+    }
+    function testMinimum() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+    function testWithdraw() public {
+        vm.expectRevert(NotOwner.selector);
+        vm.prank(address(0));
+        fundMe.withdraw();
     }
 }

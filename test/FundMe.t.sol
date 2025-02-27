86f5985d6229d5520c62bfacb1c7b994298047af
rbd3 <rabedesanaandry01@gmail.com>
2025-02-27 17:15:09 +0300

    Add more test

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -80,4 +80,27 @@ contract FundMeTest is Test {
             endingOwnerBalance
         );
     }
+
+    function testMultipleFunder() public funded {
+        uint160 numbersOfFunder = 10;
+        uint160 startingFunderIndex = 1;
+        for (uint160 i = startingFunderIndex; i < numbersOfFunder; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
 }
